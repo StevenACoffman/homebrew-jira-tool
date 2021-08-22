7c80a27522d12046b67f9842fde9908216769ee5
goreleaserbot <goreleaser@carlosbecker.com>
2021-08-22 13:24:18 -0400

    Brew formula update for jt version v0.0.8

--- Formula/jt.rb
+++ Formula/jt.rb
@@ -5,30 +5,29 @@
 class Jt < Formula
   desc "jt is a CLI tool for viewing and manipulating JIRA issues."
   homepage "https://github.com/StevenACoffman/jt"
-  version "0.0.7"
+  version "0.0.8"
   license "MIT"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/StevenACoffman/jt/releases/download/v0.0.7/jt_0.0.7_Darwin_x86_64.tar.gz"
-      sha256 "3c2a2a497f43e0b784fc8907fdc84da99150e325cee02334f8e7c97b870f8416"
+      url "https://github.com/StevenACoffman/jt/releases/download/v0.0.8/jt_0.0.8_Darwin_x86_64.tar.gz"
+      sha256 "ecfc1ba52336794826c3ae25a81cba8d3f8130aae7f5f8ff0ee9f4d2af7b9e64"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/StevenACoffman/jt/releases/download/v0.0.7/jt_0.0.7_Linux_x86_64.tar.gz"
-      sha256 "f5b2f5115604f8adce6dbf14502da4d8601d12e65aead620cab2844ed783f8a3"
+      url "https://github.com/StevenACoffman/jt/releases/download/v0.0.8/jt_0.0.8_Linux_x86_64.tar.gz"
+      sha256 "378d02a6bffb6147bf187a2f6e20b8d27021b19fd9bcb5df2892f0cff9670478"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/StevenACoffman/jt/releases/download/v0.0.7/jt_0.0.7_Linux_arm64.tar.gz"
-      sha256 "4dfe39ace23f80ecf53c74f2874bc7f5dc43c08b7413852f977ff2d70da627c7"
+      url "https://github.com/StevenACoffman/jt/releases/download/v0.0.8/jt_0.0.8_Linux_arm64.tar.gz"
+      sha256 "671a7717272b1cc857b70f3efa9510bc7573e22b35591bc683d978366ba1988a"
     end
   end
 
   def install
     bin.install "jt"
-    ...
   end
 end
